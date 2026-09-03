c594baa74d8c7118627d8a17da194bd7daea776c
Yuki Kimoto <kimoto.yuki@gmail.com>
2026-09-03 09:19:02 +0900

    Improve uv tests.

--- t/lib/SPVM/TestCase/Resource/UV.c
+++ t/lib/SPVM/TestCase/Resource/UV.c
@@ -1,8 +1,14 @@
 #include "spvm_native.h"
 
+#include "uv.h"
 
 
 int32_t SPVM__TestCase__Resource__UV__test(SPVM_ENV* env, SPVM_VALUE* stack) {
+  
+  int32_t status = 0l;
+  
+  char uv_strerror[512];
+  uv_strerror_r(status, uv_strerror, sizeof(uv_strerror));
   
   stack[0].ival = 1;
   
